91a734b58c07dc5bc5a1e0e9987cf174f77a3358
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-12-02 16:06:46 +0100

    rework of the Gulpfile: make path defintions cleaner

--- gulpfile.js
+++ gulpfile.js
@@ -12,39 +12,57 @@ var webserver = require('gulp-webserver');
 var opn = require('opn');
 var del = require('del');
 
-var basePaths = {};
-var paths = {
-  src: {
-    js: [],
-    css: [],
-    html: []
-  },
-  dst: {
-
-  },
-  clean: {
-
-  }
+var basePaths = {
+  frontend: './sqliteweb-web',
+  server: './sqliteweb-server'
 };
 
-var sourcePaths = {
-  distFiles: [
-    './sqliteweb-web/js/*.min.js',
-    './sqliteweb-web/css/*.css',
-    './sqliteweb-web/*.html',
-    './sqliteweb-web/vendor/ace/**/*',
-    '!sqliteweb-web/bower_components'
-  ],
-  filesToClean: [
-    './sqliteweb-web/js/*.js',
-    './sqliteweb-web/css/*.css'
-  ],
-  distFilesToClean: [
-    './sqliteweb-server/static/js/*.js',
-    './sqliteweb-server/static/css/*.css',
-    './sqliteweb-server/static/*.html',
-    './sqliteweb-server/static/ace/**/*.js'
-  ]
+var paths = {
+  src: {
+    js: [
+      basePaths.frontend + '/bower_components/jquery/dist/jquery.js',
+      basePaths.frontend + '/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+      basePaths.frontend + '/bower_components/lodash/dist/lodash.js',
+      basePaths.frontend + '/js/*.js',
+      '!sqliteweb-web/js/app.min.js'
+    ],
+    coffee: [
+      basePaths.frontend + '/js/app.coffee'
+    ],
+    css: [
+      basePaths.frontend + '/css/app.scss'
+    ],
+    fonts: [
+      basePaths.frontend + '/bower_components/font-awesome/fonts/*'
+    ],
+    dist: [
+      basePaths.frontend + '/js/*.min.js',
+      basePaths.frontend + '/css/*.css',
+      basePaths.frontend + '/fonts/*',
+      basePaths.frontend + '/*.html',
+      basePaths.frontend + '/vendor/ace/**/*',
+      '!sqliteweb-web/bower_components'
+    ]
+  },
+  dest: {
+    css: basePaths.frontend + '/css',
+    js: basePaths.frontend + '/js',
+    fonts: basePaths.frontend + '/fonts',
+    dist: basePaths.server + '/static'
+  },
+  clean: {
+    dev: [
+      basePaths.frontend + '/js/*.js',
+      basePaths.frontend + '/css/*.css',
+      basePaths.frontend + '/fonts/*'
+    ],
+    dist: [
+      basePaths.server + '/static/js/*.js',
+      basePaths.server + '/static/css/*.css',
+      basePaths.server + '/static/*.html',
+      basePaths.server + '/static/ace/**/*.js'
+    ]
+  }
 };
 
 var server = {
@@ -52,44 +70,43 @@ var server = {
   port: '8001'
 };
 
+// Clean local dev files
 gulp.task('clean', function(cb) {
-    gulp.src(sourcePaths.filesToClean, {read: false})
-        .pipe(clean());
+  del(paths.clean.dev);
+});
+
+// Clean dist files
+gulp.task('cleanDist', function () {
+  del(paths.clean.dist);
 });
 
 gulp.task('sass', function() {
-  gulp.src('sqliteweb-web/css/app.scss')
+  gulp.src(paths.src.css)
     .pipe(plumber())
     .pipe(sass())
     .pipe(csso())
-    .pipe(gulp.dest('sqliteweb-web/css'));
+    .pipe(gulp.dest(paths.dest.css));
 });
 
 // Lint Coffeescript files
 gulp.task('lint', function () {
-    gulp.src('./sqliteweb-web/js/app.coffee')
+    gulp.src(paths.src.coffee)
         .pipe(coffeelint())
         .pipe(coffeelint.reporter())
 });
 
 // Compile Coffeescript files
 gulp.task('coffee', function() {
-  gulp.src('./sqliteweb-web/js/app.coffee')
+  gulp.src(paths.src.coffee)
     .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(gulp.dest('./sqliteweb-web/js'));
+    .pipe(gulp.dest(paths.dest.js));
 });
 
 gulp.task('compress', function() {
-  gulp.src([
-    'sqliteweb-web/bower_components/jquery/dist/jquery.js',
-    'sqliteweb-web/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
-    'sqliteweb-web/bower_components/lodash/dist/lodash.js',
-    'sqliteweb-web/js/*.js',
-    '!sqliteweb-web/js/app.min.js'
-  ])
+  gulp.src(paths.src.js)
     .pipe(concat('app.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('sqliteweb-web/js'));
+    .pipe(gulp.dest(paths.dest.js));
 });
 
 gulp.task('watch', function() {
@@ -119,17 +136,18 @@ gulp.task('openbrowser', function() {
   opn( 'http://' + server.host + ':' + server.port );
 });
 
-// Clean dist files
-gulp.task('cleanDist', function () {
-  del(sourcePaths.distFilesToClean);
+// Copy all fonts to the public folder
+gulp.task('fonts', function() {
+  gulp.src(paths.src.fonts)
+    .pipe(gulp.dest(paths.dest.fonts));
 });
 
 // Copy all dist files to the server folder.
 gulp.task('copyDistFiles', function () {
-  gulp.src(sourcePaths.distFiles, {base: "./sqliteweb-web"})
-    .pipe(gulp.dest('./sqliteweb-server/static'));
+  gulp.src(paths.src.dist, {base: "./sqliteweb-web"})
+    .pipe(gulp.dest(paths.dest.dist));
 });
 
-gulp.task('default', ['clean', 'sass', 'lint', 'coffee', 'compress', 'webserver', 'watch', 'openbrowser']);
-gulp.task('dev', ['sass', 'lint', 'coffee', 'compress', 'cleanDist', 'copyDistFiles', 'watchDist']);
-//gulp.task('dist', ['sass', 'lint', 'coffee', 'compress', 'cleanDist', 'copyDistFiles']);
+gulp.task('default', ['clean', 'sass', 'lint', 'coffee', 'compress', 'fonts', 'webserver', 'watch', 'openbrowser']);
+gulp.task('dev', ['sass', 'lint', 'coffee', 'compress', 'fonts', 'cleanDist', 'copyDistFiles', 'watchDist']);
+gulp.task('dist', ['sass', 'lint', 'coffee', 'compress', 'fonts', 'cleanDist', 'copyDistFiles']);
